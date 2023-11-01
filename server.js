58bf148806919193b4225b6f726af1f60a255f86
PARSIVALJ <uchejesusboy@gmail.com>
2023-11-01 14:42:21 +0200

    Add form page to allow user to add new projects to their list of projects

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
+import _ from "lodash";
+import fs from "fs";
+import formidableMiddleware from "express-formidable";
+import mongodb from "mongodb";
+import path from "path";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+
 
 app.get("/", (req,res) => {
     res.render("index.ejs");
 });
+
+app.get("/form", (req,res) => {
+    res.render("form.ejs");
+})
 
 app.listen(port, () => {
     console.log("Server is running from port 3000");
