a9cb9d9157c1723454eb96aee79673f0a45344fa
PARSIVALJ <uchejesusboy@gmail.com>
2023-11-02 03:38:16 +0200

    Fix deployment issues

--- server.js
+++ server.js
@@ -192,6 +192,7 @@ app.get("/images/:filename", async (req, res) => {
 app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 
 let port = process.env.PORT;
+
 if (port == null || port == "") {
   port = 3000;
 }
