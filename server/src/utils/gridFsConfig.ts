cb347ab2eb848a0cd681eeaae1b444f24fba35f9
PARSIVALJ <uchejesusboy@gmail.com>
2024-06-22 15:40:46 +0300

    Fixed client mongoose connection bug

--- server/src/utils/gridFsConfig.ts
+++ server/src/utils/gridFsConfig.ts
@@ -1,13 +1,17 @@
 import multer from 'multer';
 import env from "./validateEnv";
 import  { conn } from '../server';
-import { GridFSBucket, Db } from 'mongodb';
+import { GridFSBucket, MongoClient } from 'mongodb';
 import { storage } from './storageSetup';
 
-export const initializeGridFs = (gfs: GridFSBucket) => {
+export const initializeGridFs = async (gfs: GridFSBucket) => {
+    const client = new MongoClient(env.MONGO_URI);
+    await client.connect();
+    const db = client.db(env.DB_NAME);
+
     conn.once("open", () => {
         console.log("Connection to database established");
-        gfs = new GridFSBucket(conn.db, {
+        gfs = new GridFSBucket(db, {
             bucketName: env.BUCKET_NAME
         })
     })
--- server/src/utils/validateEnv.ts
+++ server/src/utils/validateEnv.ts
@@ -4,5 +4,6 @@ export default cleanEnv(process.env, {
     MONGO_URI: str(),
     PORT: port(),
     BUCKET_NAME: str(),
-    CLIENT_URI: str()
+    CLIENT_URI: str(),
+    DB_NAME: str()
 })
